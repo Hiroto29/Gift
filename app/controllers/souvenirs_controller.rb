679c4619269e959efdb613839fdcd9579390664b
HirotoSugiura <yw3esaekg@outlook.jp>
2024-03-02 19:23:54 +0900

    Release to heroku

--- app/controllers/souvenirs_controller.rb
+++ app/controllers/souvenirs_controller.rb
@@ -1,51 +1,57 @@
 class SouvenirsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
-    def index
-        @souvenirs = Souvenir.all
-    search = params[:search]
-    if search.present?
-      @souvenirs = @souvenirs.joins(:user).where("body LIKE ?", "%#{search}%")
-    end
-    end
-    def new
-        @souvenir = Souvenir.new
-      end
-    
-      def create
-        souvenir = Souvenir.new(souvenir_params)
-        tweet.user_id = current_user.id
-        if souvenir.save!
-          redirect_to :action => "index"
-        else
-          redirect_to :action => "new"
-        end
-      end
-      def show
-        @souvenir = Souvenir.find(params[:id])
-      end
-      def edit
-        @souvenir = Souvenir.find(params[:id])
-      end
-      def update
-        souvenir = Souvenir.find(params[:id])
-        if souvenir.update(souvenir_params)
-          redirect_to :action => "show", :id => souvenir.id
-        else
-          redirect_to :action => "new"
-        end
-      end
-      def destroy
-        souvenir = Souvenir.find(params[:id])
-        souvenir.destroy
-        redirect_to action: :index
-      end
 
-      def ichiosi
-        @souvenirs = Souvenir.where(user_id: 1)
+    def index
+        @souvenirs = Souvenir.where.not(user_id: 2)
+        search = params[:search]
+      if search.present?
+        @souvenirs = @souvenirs.joins(:user).where("name LIKE ?", "%#{search}%")
       end
+    end
+
+    def new
+      @souvenir = Souvenir.new
+    end
     
-      private
-      def souvenir_params
-        params.require(:souvenir).permit(:name, :prefecture, :adress, :about, :image, :body, :lat, :lng)
+    def create
+      souvenir = Souvenir.new(souvenir_params)
+      souvenir.user_id = current_user.id
+      if souvenir.save!
+        redirect_to :action => "index"
+      else
+        redirect_to :action => "new"
       end
+    end
+
+    def show
+      @souvenir = Souvenir.find(params[:id])
+    end
+    
+    def edit
+      @souvenir = Souvenir.find(params[:id])
+    end
+    
+    def update
+      souvenir = Souvenir.find(params[:id])
+      if souvenir.update(souvenir_params)
+        redirect_to :action => "show", :id => souvenir.id
+      else
+        redirect_to :action => "new"
+      end
+    end
+
+    def destroy
+      souvenir = Souvenir.find(params[:id])
+      souvenir.destroy
+      redirect_to action: :index
+    end
+
+    def ichiosi
+      @souvenirs = Souvenir.where(user_id: 2)
+    end
+    
+    private
+    def souvenir_params
+      params.require(:souvenir).permit(:name, :prefecture, :adress, :about, :image, :lat, :lng)
+    end
 end
--- app/models/souvenir.rb
+++ app/models/souvenir.rb
@@ -1,3 +1,5 @@
 class Souvenir < ApplicationRecord
+    belongs_to :user 
     mount_uploader :image, ImageUploader
+    
 end
